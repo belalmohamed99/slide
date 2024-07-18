3825245c7c06b5d12eedd6b506d0f045cda26d93
Belal mohamed <belal2003mohamed@gmail.com>
2024-07-18 12:48:28 +0300

    edit js files

--- index.js
+++ index.js
@@ -9,14 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
   nextButton.addEventListener("click", () => {
     if (counter >= images.length - 1) return;
-    carouselSlide.style.transition = "transform 0.5s ease-in-out";
     counter++;
     carouselSlide.style.transform = `translateX(${-size * counter}px)`;
   });
 
   prevButton.addEventListener("click", () => {
     if (counter <= 0) return;
-    carouselSlide.style.transition = "transform 0.5s ease-in-out";
     counter--;
     carouselSlide.style.transform = `translateX(${-size * counter}px)`;
   });
